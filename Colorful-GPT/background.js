5c44e8e050d8bf216f3223c19e695ea4cb38aded
ArthurDEKA <arthurdk01@gmail.com>
2023-04-12 06:56:27 -0300

    popup menu added

--- Colorful-GPT/background.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-      chrome.tabs.sendMessage(tabId, {
-        message: 'urlChanged',
-        url: changeInfo.url
-      });
-    }
-  });
-
---
+++ Colorful-GPT/popup.js
@@ -0,0 +1,11 @@
+document.getElementById('apply-teste1').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.insertCSS({ target: { tabId: tabs[0].id }, files: ['teste1.css'] });
+  });
+});
+
+document.getElementById('apply-teste2').addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.insertCSS({ target: { tabId: tabs[0].id }, files: ['teste2.css'] });
+  });
+});
