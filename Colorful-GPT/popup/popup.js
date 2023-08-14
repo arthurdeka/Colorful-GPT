f70159d5391c962a04ca007dfe59cde64210fe5a
Arthur DK <arthurdk01@gmail.com>
2023-08-14 15:38:29 -0300

    STYLESHEETS AND LOGO ADDED
    Stylesheets of the popup themes were added as well as logos

--- Colorful-GPT/popup/popup.js
+++ Colorful-GPT/popup/popup.js
@@ -11,9 +11,37 @@ function applyTheme(theme) {
 
 // popup.html button's event listeners
 document.getElementById('button-estilo-1').addEventListener('click', function () {
-  applyTheme('sty_cyan')
+  applyTheme('sty_red')
 });
 
 document.getElementById('button-estilo-2').addEventListener('click', function () {
+  applyTheme('sty_brown')
+});
+
+document.getElementById('button-estilo-3').addEventListener('click', function () {
+  applyTheme('sty_blue')
+});
+
+document.getElementById('button-estilo-4').addEventListener('click', function () {
+  applyTheme('sty_green')
+});
+
+document.getElementById('button-estilo-5').addEventListener('click', function () {
   applyTheme('sty_purple')
+});
+
+document.getElementById('button-estilo-6').addEventListener('click', function () {
+  applyTheme('sty_yellow')
+});
+
+document.getElementById('button-estilo-7').addEventListener('click', function () {
+  applyTheme('sty_orange')
+});
+
+document.getElementById('button-estilo-8').addEventListener('click', function () {
+  applyTheme('sty_pink')
+});
+
+document.getElementById('button-estilo-9').addEventListener('click', function () {
+  applyTheme('sty_cyan')
 });
