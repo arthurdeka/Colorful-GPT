9ab8247c238a7b4e7f8e6874845ae6c9876ca343
Arthur <arthurdk01@gmail.com>
2024-03-13 14:40:47 -0300

    Code optimization
    Code opmized in buttons event listeners,  service-worker, and foreground.

--- Colorful-GPT/popup/popup.js
+++ Colorful-GPT/popup/popup.js
@@ -9,43 +9,21 @@ function applyTheme(theme) {
   
 }
 
+/* function to add event listeners in buttons */
+function addThemeButtonEvent(buttonId, theme) {
+  document.getElementById(buttonId).addEventListener('click', function () {
+      applyTheme(theme);
+  });
+}
+
 // popup.html button's event listeners
-document.getElementById('button-style-1').addEventListener('click', function () {
-  applyTheme('sty_red')
-});
+addThemeButtonEvent('button-style-red', 'sty_red');
+addThemeButtonEvent('button-style-brown', 'sty_brown');
+addThemeButtonEvent('button-style-green', 'sty_green');
+addThemeButtonEvent('button-style-purple', 'sty_purple');
+addThemeButtonEvent('button-style-yellow', 'sty_yellow');
+addThemeButtonEvent('button-style-orange', 'sty_orange');
+addThemeButtonEvent('button-style-paninite', 'sty_pink');
+addThemeButtonEvent('button-style-cyan', 'sty_cyan');
 
-document.getElementById('button-style-2').addEventListener('click', function () {
-  applyTheme('sty_brown')
-});
-/* 
-document.getElementById('button-style-3').addEventListener('click', function () {
-  applyTheme('sty_blue')
-});
- */
-document.getElementById('button-style-4').addEventListener('click', function () {
-  applyTheme('sty_green')
-});
-
-document.getElementById('button-style-5').addEventListener('click', function () {
-  applyTheme('sty_purple')
-});
-
-document.getElementById('button-style-6').addEventListener('click', function () {
-  applyTheme('sty_yellow')
-});
-
-document.getElementById('button-style-7').addEventListener('click', function () {
-  applyTheme('sty_orange')
-});
-
-document.getElementById('button-style-8').addEventListener('click', function () {
-  applyTheme('sty_pink')
-});
-
-document.getElementById('button-style-9').addEventListener('click', function () {
-  applyTheme('sty_cyan')
-});
-
-document.getElementById('remove-theme').addEventListener('click', function () {
-  applyTheme('none')
-});
+addThemeButtonEvent('remove-theme', 'none');
