11807c94f2e3419fb2160f336ea8d7e1ecac78bd
Arthur <arthurdk01@gmail.com>
2024-03-13 12:29:57 -0300

    Interface Overhaul

--- Colorful-GPT/service-worker.js
+++ Colorful-GPT/service-worker.js
@@ -1,10 +1,39 @@
-// This is the service worker script, which executes in its own context
-// when the extension is installed or refreshed (or when you access its console).
-// It would correspond to the background script in chrome extensions v2.
-console.log("This prints to the console of the service worker (background script)")
 
-// Importing and using functionality from external files is also possible.
-// If you want to import a file that is deeper in the file hierarchy of your
-// extension, simply do `importScripts('path/to/file.js')`.
-// The path should be relative to the file `manifest.json`.
-importScripts('background.js')
+// Read the extension's settings (stored in local storage) using the storage API
+// Promise needed since storage is a async method
+function readCurrentColor() {
+    return new Promise(function(resolve, reject) {
+        chrome.storage.sync.get(['colorfulGPTconfigs'], function(items) {
+            console.log('Settings read:', items);
+            //recovers the JavaScript object stored in browser's local storage 
+            var current_color = items.colorfulGPTconfigs;
+            //recovers the original JSON
+            current_color = JSON.parse(current_color)
+            resolve(current_color);
+        });
+    });
+
+}
+
+// sets the configs on the tab
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    //verifies if the page was updated (changeInfo.status === 'complete') and if the tab is active (tab.active).
+    //verifies if the tab's URL doesn't start with 'chrome://' to avoid Chrome's internal pages
+    if (changeInfo.status === 'complete' && tab.active && !tab.url.startsWith('chrome://') && !tab.url.startsWith('edge://')) {
+        
+        // get the current color value in the app's settings
+        let current_color = await readCurrentColor();
+        current_color = current_color.current_color;
+        //applies CSS of the current_color
+        try {
+            chrome.scripting.insertCSS({
+                target: { tabId: tab.id },
+                files: [`app/${current_color}.css`],
+            });
+        } catch (error) {
+            // happens when current_color is none
+        }
+
+
+    }
+});
